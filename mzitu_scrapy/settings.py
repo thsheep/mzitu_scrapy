3df527f7c4d9270cf810a0712e271f79df2bd90c
thsheep <thsheep@live.com>
2017-05-03 15:49:35 +0800

    更改了下载速度限制

--- mzitu_scrapy/settings.py
+++ mzitu_scrapy/settings.py
@@ -22,12 +22,12 @@ NEWSPIDER_MODULE = 'mzitu_scrapy.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 6
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
